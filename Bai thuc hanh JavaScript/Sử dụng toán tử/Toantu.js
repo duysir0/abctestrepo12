a506980aa21451b825ec445a807399ae8a1178d0
Thai Duy <hommail3009@gmail.com>
2022-05-06 10:21:32 +0700

    thêm Sử dụng toán tử

--- Bai thuc hanh JavaScript/Sử dụng toán tử/Toantu.js
+++ Bai thuc hanh JavaScript/Sử dụng toán tử/Toantu.js
@@ -2,4 +2,6 @@
 let inputWidth = +prompt("Nhập chiều rộng");
 let inputHeight = +prompt("Nhập chiều dài");
 let dtich = inputWidth * inputHeight;
-document.write("Diện tích là: " + dtich);
+document.write("Chiều rộng nhập: " + inputWidth)
+document.write("<br>Chiều dài nhập: " + inputWidth)
+document.write("<br>Diện tích là: " + dtich)
