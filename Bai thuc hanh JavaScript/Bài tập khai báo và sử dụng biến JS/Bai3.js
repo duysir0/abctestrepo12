5ac959ccfdc85ab7805bb848f8d162da35fddfc4
Thai Duy <hommail3009@gmail.com>
2022-05-06 10:00:10 +0700

    thêm Bài tập khai báo và sd biến JS.html

---
+++ Bai thuc hanh JavaScript/Bài tập khai báo và sử dụng biến JS/Bai3.js
@@ -0,0 +1,8 @@
+let a= +prompt('Hãy nhập số a');
+let b= +prompt('Hãy nhập số b');
+let xetdk = a%b === 0;   //Xet  a chia b ra so du hay ko (True - False)
+
+document.write('a=' + a)
+
+document.write('<br>b=' + b)
+alert("a là bội số của b? " + (xetdk))
--- Bai thuc hanh JavaScript/FileJsOnClick.js
+++ Bai thuc hanh JavaScript/FileJsOnClick.js
@@ -1,6 +1,13 @@
-const gichadc = document.getElementById("button1")
-    document.addEventListener("click", onClick)
+//const gichadc = document.getElementById("button1")
+    //document.addEventListener("click", onClick)
+// function onClick() {
+//     document.getElementById("button1").innerHTML = Date()
+// }
+let gichadc2 = document.getElementById("button3")
+            document.addEventListener("click",button3)
 
-function onClick() {
-    document.getElementById("button1").innerHTML = Date()
-}
+function button3() {
+    document.getElementById("button3").innerHTML = Date()
+
+    }
+
