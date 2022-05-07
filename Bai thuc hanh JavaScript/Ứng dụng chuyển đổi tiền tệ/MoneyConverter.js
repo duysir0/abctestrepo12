626524fdb017a31101301f203e369ba5cc0e487d
TháiDuy <35.26.006@ajc.edu.vn>
2022-05-08 00:55:24 +0700

    abc

--- Bai thuc hanh JavaScript/Ứng dụng chuyển đổi tiền tệ/MoneyConverter.js
+++ Bai thuc hanh JavaScript/Ứng dụng chuyển đổi tiền tệ/MoneyConverter.js
@@ -1,23 +1,36 @@
 let Amount = document.getElementById('giatriAmount').value;
 let fromCurrency = document.getElementById('giatriFromCurr').value;
 let toCurrency = document.getElementById('giatriToCurr').value;
+let currencyRate = {
+    "VND": 1,
+    "USD": 23
+};
 
-fromCurrency.addEventListener("change",smartCurrencySelect)
+function convertCurrency() {
+    let rateHientai = currencyRate * fromCurrency;
+    let rateChuyentoi = currencyRate * toCurrency;
+    let tienHientai = Amount * rateHientai;
+    let tiendaQuyDoi = tienHientai / rateChuyentoi;
 
-function smartCurrencySelect() {
-    if (fromCurrency.value === "VND") {toCurrency.value = "USD";}
-    else {toCurrency.value = "VND";}
+    return tiendaQuyDoi;
+    }
 
-// Ý tưởng: function (khi chọn 1 bên là VND > Bên còn lại tự set thành giá trị còn lại)
+    let tongtiendaQuydoi = convertCurrency();
+    let ketQua = document.getElementById('Result').innerHTML =
+    (` ${tongtiendaQuydoi}`)
+// Ý tưởng: function (khi chọn 1 bên là VND > Bên còn lại tự set thành giá trị còn lại
+
+// fromCurrency.addEventListener('change',smartCurrencySelect())
+
+// if(fromCurrency=="USD") {toCurrency="VND"};
+// else {toCurrency="USD"};
+    // if (fromCurrency.value === "VND") {toCurrency.value.set = "USD";}
+    // else {toCurrency.value = "VND";}
 
 
 
-}
 
-function InnConvert() {
-    console.log(Amount)
-    console.log(fromCurrency)
-    console.log(toCurrency)
+
 }
 //{
 //
