9ad33fd355a6832ef2bb9e65eabdfa86202b0eb2
Thai Duy <hommail3009@gmail.com>
2022-05-09 19:13:11 +0700

    Thêm finished Ứng dụng kiểm tra năm nhuận

--- Bai thuc hanh JavaScript/Kiểm tra năm nhuận/LeapYeahTest.js
+++ Bai thuc hanh JavaScript/Kiểm tra năm nhuận/LeapYeahTest.js
@@ -1,17 +1,8 @@
-let nhapNam = +prompt('Xin hãy nhập năm cần xét')
-document.getElementById('content1').innerHTML =
-    (`Năm đã nhập: ${nhapNam}`)
-if (nhapNam % 4 == 0 && nhapNam % 100 != 0) {
-    document.getElementById('result').innerHTML = (`Không`);
-    else if (nhapNam % 100 == 0 && nhapNam
 
 
 
 
 
-
-    document.getElementById('result').innerHTML = (`Có`)
-} else {document.getElementById('result').innerHTML = (`Không`)
 
 
 }
