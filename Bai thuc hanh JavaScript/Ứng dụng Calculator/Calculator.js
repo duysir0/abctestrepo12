ef3b45331862f071385673a76376a7d7ccf401dc
Thai Duy <hommail3009@gmail.com>
2022-05-09 15:42:32 +0700

    Hoàn thành Ứng dụng chuyển đổi tiền tệ

--- Bai thuc hanh JavaScript/Ứng dụng Calculator/Calculator.js
+++ Bai thuc hanh JavaScript/Ứng dụng Calculator/Calculator.js
@@ -2,27 +2,27 @@
 // let So2 = Number(document.getElementById('num2').value);
 //
 // //Thiet lap function cho button
-// function functionAdd() {
-//     let So1 = +document.getElementById('num1').value;
-//     let So2 = +document.getElementById('num2').value;
-//     KetQua = So1 + So2
-//     document.getElementById('result').innerHTML = KetQua
-// }
-// function functionSubtract() {
-//     let So1 = +document.getElementById('num1').value;
-//     let So2 = +document.getElementById('num2').value;
-//     KetQua = So1 - So2
-//     document.getElementById('result').innerHTML = KetQua
-// }
-// function functionMulti() {
-//     let So1 = +document.getElementById('num1').value;
-//     let So2 = +document.getElementById('num2').value;
-//     KetQua = So1 * So2
-//     document.getElementById('result').innerHTML = KetQua
-// }
-// function functionDivide() {
-//     let So1 = +document.getElementById('num1').value;
-//     let So2 = +document.getElementById('num2').value;
-//     KetQua = So1 / So2
-//     document.getElementById('result').innerHTML = KetQua
-// }
+function functionAdd() {
+    let So1 = +document.getElementById('num1').value;
+    let So2 = +document.getElementById('num2').value;
+    KetQua = So1 + So2
+    document.getElementById('result').innerHTML = KetQua
+}
+function functionSubtract() {
+    let So1 = +document.getElementById('num1').value;
+    let So2 = +document.getElementById('num2').value;
+    KetQua = So1 - So2
+    document.getElementById('result').innerHTML = KetQua
+}
+function functionMulti() {
+    let So1 = +document.getElementById('num1').value;
+    let So2 = +document.getElementById('num2').value;
+    KetQua = So1 * So2
+    document.getElementById('result').innerHTML = KetQua
+}
+function functionDivide() {
+    let So1 = +document.getElementById('num1').value;
+    let So2 = +document.getElementById('num2').value;
+    KetQua = So1 / So2
+    document.getElementById('result').innerHTML = KetQua
+}
