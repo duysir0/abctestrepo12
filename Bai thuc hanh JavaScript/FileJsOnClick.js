0e45a747bfebd56fd58790599ab02f8f6b389a61
duysir0 <hommail3009@gmail.com>
2022-05-10 19:42:44 +0700

    Update FileJsOnClick.js

--- Bai thuc hanh JavaScript/FileJsOnClick.js
+++ Bai thuc hanh JavaScript/FileJsOnClick.js
@@ -1,8 +1,3 @@
-//const gichadc = document.getElementById("button1")
-    //document.addEventListener("click", onClick)
-// function onClick() {
-//     document.getElementById("button1").innerHTML = Date()
-// }
 let gichadc2 = document.getElementById("button3")
             document.addEventListener("click",button3)
 
