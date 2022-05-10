7d6169b05c5639f07bc394e31b68906f83b2dbc0
Thai Duy <hommail3009@gmail.com>
2022-05-10 09:16:20 +0700

    thêm BMI hiện tại cho ứng dụng xét chỉ số cân nặng

--- Bai thuc hanh JavaScript/Tính chỉ số cân nặng của cơ thể/BMI.js
+++ Bai thuc hanh JavaScript/Tính chỉ số cân nặng của cơ thể/BMI.js
@@ -4,6 +4,7 @@ function abc()
     let Height = document.getElementById('inHeight').value;
     let bmi = Weight / ( Height ** 2 );
 
+    document.getElementById('BMIhientai').innerHTML = (`BMI hiện tại: ${bmi.toFixed(2)}`)
     if (bmi < 18)
         document.getElementById("result").innerHTML = (`Bộ Xương`);
     else if (bmi < 25.0)
@@ -12,5 +13,5 @@ function abc()
         document.getElementById("result").innerHTML =  (`Hơi béo`);
     else if (bmi >= 30.0)
         document.getElementById("result").innerHTML =  (`Khủng long`)
-    else ;
+    else `null`;
 }
