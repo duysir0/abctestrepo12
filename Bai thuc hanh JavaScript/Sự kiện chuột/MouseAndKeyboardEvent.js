5bc57211178ccc51aec9a9a3e4c13d5f140a411c
Thai Duy <hommail3009@gmail.com>
2022-05-11 08:32:49 +0700

    Sửa Chuột và Phím Event

--- Bai thuc hanh JavaScript/Sự kiện chuột/MouseAndKeyboardEvent.js
+++ Bai thuc hanh JavaScript/Sự kiện chuột/MouseAndKeyboardEvent.js
@@ -19,21 +19,25 @@ function moveDown() {
     imgObj.style.top = parseInt(imgObj.style.top) + 10 + 'px';
 }
 
-//Function di chuyển bằng phím WASD
+//Function di chuyển bằng bàn phím phím
 function dichuyenPhim() {
     imgObj = document.getElementById('myImage')
     switch (event.keyCode) {
-        case 65:   //phimA sang trái 10 pixel
-            imgObj.style.left = parseInt(imgObj.style.left) - 10 + 'px'
+        case 65:   //phím A
+        case 37:    //phím mũi tên Trái
+            moveLeft()     //gọi function moveLeft() - di chuyên hình sang trái 10 pixel
             break;
-        case 68:    //phimD sang phải 10 pixel
-            imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px'
+        case 68:    //phím D
+        case 39:    //phim mũi tên Phải
+            moveRight()   //gọi function moveRight() - di chuyên hình sang phải 10 pixel
             break;
-        case 87:    //phimW lên trên 10 pixel
-            imgObj.style.top = parseInt(imgObj.style.top) - 10 + 'px'
+        case 87:    //phím W lên trên 10 pixel
+        case 38:    //phím mũi tên Up
+            moveUp() //gọi function moveUp() - di chuyên hình lên trên 10 pixel
             break;
-        case 83:    //phimS xuống dưới 10 pixel
-            imgObj.style.top = parseInt(imgObj.style.top) + 10 + 'px'
+        case 83:    //phím S xuống dưới 10 pixel
+        case 40:    //phím mũi tên Down
+            moveDown()     //gọi function moveDown() - di chuyên hình xuống 10 pixel
             break;
     }
 }
